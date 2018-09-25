db64108b4e77bc77585050468b498dd3644b9ef0
ranix <reneciwuitl@gmial.com>
2018-09-25 10:58:56 -0500

    add Login module

--- arkham_tattoo_studio/urls.py
+++ arkham_tattoo_studio/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path('contacto', views.contact, name='contact'),
     path('login', auth_views.LoginView.as_view(), name='login'),
     path('logout', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('change-password', auth_views.PasswordChangeView.as_view(), name='change_password'),
 
     # Quotation
     path('cotizaciones/', include('quotation.urls')),
